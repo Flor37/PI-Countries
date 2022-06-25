e36c714bd99b6b3bbef8f2e2e2b0d12c0a2bf373
unknown <choymedina1989@gmail.com>
2022-06-24 20:38:51 -0500

    db finished and firts route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,28 @@
 const { Router } = require('express');
+const services = require('./services_routes');
+const { Country, Activity } = require('../db');
+const axios = require('axios') 
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
-const router = Router();
-
+const router = Router(); 
+ 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get('/countries', async (req, res) => {   
+    const apiCountry = await services.getApiInf()
+    const countries = apiCountry.map(e => e) 
+    countries.forEach(c => { 
+        Country.findOrCreate({   
+            where: c     
+        })                         
+    }) 
+    const allCountries = await Country.findAll()
+    res.send(allCountries)
+})
 
 module.exports = router;
+   
