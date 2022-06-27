7b39d38dba3256083498aae69c36fafa0eecc01a
unknown <choymedina1989@gmail.com>
2022-06-27 17:48:02 -0500

    all routes finished

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -21,7 +21,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-    seasson:{
+    seasson:{ 
       type: DataTypes.ENUM('Spring', 'Summer', 'Fall', 'Winter'),
       allowNull: false
     }
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,28 +1,63 @@
 const { Router } = require('express');
 const services = require('./services_routes');
 const { Country, Activity } = require('../db');
-const axios = require('axios') 
+const axios = require('axios');
+const e = require('express');
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
-const router = Router(); 
- 
+const router = Router();
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get('/countries', async (req, res) => {   
-    const apiCountry = await services.getApiInf()
-    const countries = apiCountry.map(e => e) 
-    countries.forEach(c => { 
-        Country.findOrCreate({   
-            where: c     
-        })                         
-    }) 
-    const allCountries = await Country.findAll()
-    res.send(allCountries)
+router.get('/countries', async (req, res) => {
+    const name = req.query.name    
+    const theCountries = await services.getDBInf()
+    const allCountries = await theCountries.map(c => {
+        return {
+            flag: c.flag,
+            name: c.name,
+            contient: c.continent
+        }
+    })
+    if(name){
+        let countryName = await allCountries.filter(c => c.name.toLowerCase().includes(name.toLowerCase()))
+        countryName.length ?
+        res.status(200).send(countryName) :
+        res.status(404).send('Country not found')
+    } else {
+    res.status(200).send(allCountries)
+    }
+})
+
+router.get('/countries/:id', async(req, res) =>{
+    const id = req.params.id
+    const allCountries = await services.getDBInf()
+    if(id){
+        let countryId = await allCountries.find(c => c.id == id)
+        countryId ?
+        res.status(200).send(countryId) :
+        res.status(404).send('Country not found')
+    }
+})
+
+router.post('/activities', async (req, res) => {
+    let {
+        name, difficulty, duration, seasson, country
+    } = req.body
+    let activityCreated = await Activity.create({
+        name, difficulty, duration, seasson
+    })
+    let countryDB = await Country.findAll({
+        where:{
+            name: country
+        }
+    })
+    activityCreated.addCountry(countryDB)
+    res.send('Activity created succesfully')
 })
 
 module.exports = router;
-   
